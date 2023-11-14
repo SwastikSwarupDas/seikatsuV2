233b9f57d9793abb6734336193a166221d2c1c14
SwastikSwarupDas <72328862+SwastikSwarupDas@users.noreply.github.com>
2023-11-14 09:00:28 +0530

    change to auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject,Observable} from 'rxjs';
+import { ApiService,user } from './api.service';
+import {first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private isAuthenticated:boolean=false;
+  private username:string = '';
   private userType:string='';
   private authChangedSource: Subject<void> = new Subject<void>();
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private apiService:ApiService) { }
 
   loginSuccess(userType:string){
-    this.isAuthenticated = true;
-    this.userType = userType;
-    this.authChangedSource.next();
-    this.router.navigate(['']);
+    this.apiService.user$.pipe(first()).subscribe((users:user[])=>{
+      const latestUser = users[users.length-1];
+      this.username = latestUser.username;
+      this.userType = latestUser.usertype;
+      this.authChangedSource.next();
+      this.router.navigate(['']);
+    });
   }
 
   isAuthenticatedUser():boolean{
     return this.isAuthenticated;
+  }
+
+  getUsername():string{
+    return this.username;
   }
 
   getUserType():string{
@@ -30,6 +40,7 @@ export class AuthService {
   logout(){
     this.isAuthenticated = false;
     this.userType = '';
+    this.username = '';
     this.authChangedSource.next();
     this.router.navigate(['login'])
   }
